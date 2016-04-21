a3f479a2b833e3882041157892ff1ae705702b66
vickykatara <vicky.p.katara@gmail.com>
2016-04-21 05:56:57 -0400

    Fr

--- src/Hello.java
+++ src/Hello.java
@@ -7,7 +7,7 @@ public class Hello
    public static void main( String[] args )
    {
       
-      System.out.println(new File("\"C:\\Users\\Vicky Katara\\Documents\\NetBeansProjects\\FaceRecLogOff\\haarcascade_frontalface_alt.xml").getPath());
+      //System.out.println(new File("\"C:\\Users\\Vicky Katara\\Documents\\NetBeansProjects\\FaceRecLogOff\\haarcascade_frontalface_alt.xml").getPath());
       System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
       Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
       System.out.println( "mat = " + mat.dump() );
